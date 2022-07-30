2ac21e7e1a2da58af0df4671ee6fc6f65e6618a7
Peter Kingori <kpndegwa@gmail.com>
2022-07-30 20:51:31 +0300

    Add fragments and NavHostFragment.

---
+++ app/src/main/java/com/pkndegwa/newsapp/ui/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.pkndegwa.newsapp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.pkndegwa.newsapp.R
+import com.pkndegwa.newsapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
+        binding.bottomNavigationView.setupWithNavController(navHostFragment.navController)
+    }
+}
---
+++ app/src/main/java/com/pkndegwa/newsapp/ui/fragments/BreakingNewsFragment.kt
@@ -0,0 +1,7 @@
+package com.pkndegwa.newsapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.pkndegwa.newsapp.R
+
+class BreakingNewsFragment: Fragment(R.layout.fragment_breaking_news) {
+}
