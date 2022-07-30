7ec79dd14ad0bb6190502c694e3458fc52f0a404
Peter Kingori <kpndegwa@gmail.com>
2022-07-30 22:47:22 +0300

    Setup Retrofit to be able to make network requests.

---
+++ app/src/main/java/com/pkndegwa/newsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.pkndegwa.newsapp.util
+
+import com.pkndegwa.newsapp.BuildConfig
+
+class Constants {
+    companion object {
+        const val APIKEY = BuildConfig.API_KEY
+        const val BASE_URL = "https://newsapi.org/"
+    }
+}
